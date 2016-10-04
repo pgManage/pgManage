15fac2b935090b34036a54b126c39c2aba3cb3b3
Nunzio Tocci <nunzio@tocci.org>
2016-10-04 10:04:13 -0500

    add missing semi-colons

--- postage_electron/main.js
+++ postage_electron/main.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const electron = require('electron')
+const path = require('path');
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 const child_process = require('child_process');
 var proc = child_process.spawn(
@@ -37,13 +37,13 @@ let mainWindow
 
 function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({ width: 1024, height: 768 })
+	mainWindow = new BrowserWindow({ width: 1024, height: 768 });
 
 	// Open the DevTools.
-	//mainWindow.webContents.openDevTools()
+	//mainWindow.webContents.openDevTools();
 
 	// and load the index.html of the app.
-	mainWindow.loadURL('http://127.0.0.1:8080/postage/index.html', { 'extraHeaders': 'pragma: no-cache\n' })
+	mainWindow.loadURL('http://127.0.0.1:8080/postage/index.html', { 'extraHeaders': 'pragma: no-cache\n' });
 
 	mainWindow.setMenu(null);
 
@@ -53,30 +53,30 @@ function createWindow() {
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
 		mainWindow = null
-	})
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
-})
+});
 
 app.on('activate', function () {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
-		createWindow()
+		createWindow();
 	}
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
