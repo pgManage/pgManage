31ece819b4108ded7c31f09028ab52784929522f
michaeltocci <michael@tocci.org>
2016-10-08 21:07:11 -0500

    update js

--- envelope/app/developer_g/greyspots-1.1.1/js/004-doT.js
+++ envelope/app/developer_g/greyspots-1.1.1/js/004-doT.js
@@ -13,27 +13,30 @@ f.encodeHTMLSource=function(b){var a={"&":"&#38;","<":"&#60;",">":"&#62;",'"':"&
 b.toString().replace(d,function(b){return a[b]||b}):b}}; // replaced :"" with :b so that if you put a zero in encodehtml you get a zero out of encodehtml
 
 
-m=function(){return this||(0,eval)("this")}();"undefined"!==typeof module&&module.exports?module.exports=f:"function"===typeof define&&define.amd?define(function(){return f}):m.doT=f;
+m=function(){return this||(0,eval)("this")}();
+
+"undefined"!==typeof module&&module.exports?module.exports=f:"function"===typeof define&&define.amd?define(function(){return f}):m.doT=f;
 
 
 
 var r={start:"'+(",end:")+'",startencode:"'+encodeHTML("},s={start:"';out+=(",end:");out+='",startencode:"';out+=encodeHTML("},h=/$^/;f.template=function(b,a,d){a=a||f.templateSettings;var n=a.append?r:s,c,e=0,g;b=a.use||a.define?p(a,b,d||{}):b;b=("var out='"+(a.strip?
 b.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g," ").replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g,""):b).replace(/'|\\/g,"\\$&").replace(a.interpolate||h,function(b,a){return n.start+k(a)+n.end}).replace(a.encode||h,function(b,a){c=!0;return n.startencode+k(a)+n.end}).replace(a.conditional||h,function(b,a,c){return a?c?"';}else if("+k(c)+"){out+='":"';}else{out+='":c?"';if("+k(c)+"){out+='":"';}out+='"}).replace(a.iterate||h,function(b,a,c,d){if(!a)return"';} } out+='";e+=1;g=d||"i"+e;a=k(a);return"';var arr"+
 e+"="+a+";if(arr"+e+"){var "+c+","+g+"=-1,l"+e+"=arr"+e+".length-1;while("+g+"<l"+e+"){"+c+"=arr"+e+"["+g+"+=1];out+='"}).replace(a.evaluate||h,function(a,b){return"';"+k(b)+"out+='"})+"';return out;").replace(/\n/g,"\\n").replace(/\t/g,"\\t").replace(/\r/g,"\\r").replace(/(\s|;|\}|^|\{)out\+='';/g,"$1").replace(/\+''/g,"");c&&(a.selfcontained||!m||m._encodeHTML||(m._encodeHTML=f.encodeHTMLSource(a.doNotSkipEncoded)),b="var encodeHTML = typeof _encodeHTML !== 'undefined' ? _encodeHTML : ("+f.encodeHTMLSource.toString()+
-"("+(a.doNotSkipEncoded||"")+"));"+b);try{return new Function(a.varname,b)}catch(q){throw"undefined"!==typeof console&&console.log("Could not create a template function: "+b),q;}};f.compile=function(b,a){return f.template(b,null,a)}})();
+"("+(a.doNotSkipEncoded||"")+"));"+b);try{return new Function(a.varname,b)}catch(q){throw"undefined"!==typeof console&&console.log("Could not create a template function: "+b),q;}};f.compile=function(b,a){return f.template(b,null,a)}
 
-// we use doT.js in the global space, but sometimes module may be
-//      defined (electron defines module automatically) so we need to copy dot to the global namespace
-(function () {
-    if (!window.doT && window.module) {
-        window.doT = {};
+
+    // we use doT.js in the global space, but sometimes module may be
+    //      defined (electron defines module automatically) so we need to copy dot to
+    //      the global namespace
+    if (!window.doT) { // && window.module
+        window.doT = f;
         
-        window.doT.encodeHTMLSource = module.exports.encodeHTMLSource;
-        window.doT.compile = module.exports.compile;
-        window.doT.templateSettings = module.exports.templateSettings;
-        window.doT.template = module.exports.template;
+        //window.doT.encodeHTMLSource = module.exports.encodeHTMLSource;
+        //window.doT.compile = module.exports.compile;
+        //window.doT.templateSettings = module.exports.templateSettings;
+        //window.doT.template = module.exports.template;
     }
-}());
+})();
 
 
 doT.templateSettings = {
