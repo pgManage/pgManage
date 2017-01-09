37a97129b4cbaccf08f6112c1c464e3b4e5107a2
Nunzio Tocci <nunzio@tocci.org>
2017-01-09 16:55:22 -0600

    include libpq on mac electron version

--- postage_electron/update.sh
+++ postage_electron/update.sh
@@ -5,6 +5,17 @@ set -e
 
 rm -rf app/postage
 mkdir app/postage
-cp ../postage/postage app/postage/postage
 cp -r ../postage/web_root app/postage/web_root
 cp -r ../postage/config app/postage/config
+
+if test $(uname -s) = "Darwin"; then
+	clang	-L/usr/local/lib -lm -L/usr/local/opt/gettext/lib \
+		../dependencies/libressl/tls/.libs/libtls.a ../dependencies/libressl/ssl/.libs/libssl.a \
+		../dependencies/libressl/crypto/.libs/libcrypto.a \
+		mac_lib/libpq.a -Wall -Wextra -Wconversion -O3 -g \
+		../common/*.o ../util/*.o ../postage/*.o ../db_framework_pq/*.o \
+		-o app/postage/postage
+else
+	cp ../postage/postage app/postage/postage
+fi
+
