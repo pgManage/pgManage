d11f5b38145cfa871fd007d862e34ec649c74c57
Nunzio Tocci <nunzio@tocci.org>
2017-03-03 11:15:41 -0600

    add PGPASS editor (closes workflowproducts/postage#125), make external link open in default browser (closes #128)

--- postage_electron/app/main.js
+++ postage_electron/app/main.js
@@ -52,6 +52,13 @@ function spawnPostage() {
 	});
 }
 
+function handleRedirect(e, url) {
+	if (url.indexOf('http://127.0.0.1:' + int_postage_port) === -1) {
+		e.preventDefault()
+		electron.shell.openExternal(url);
+	}
+}
+
 var int_postage_port = null;
 
 try {
@@ -93,9 +100,11 @@ require('electron-context-menu')({});
 let mainWindows = [];
 let configWindow = null;
 let connectionWindow = null;
+let pgpassWindow = null;
 let mainWindowState = null;
 let configWindowState = null;
 let connectionWindowState = null;
+let pgpassWindowState = null;
 
 ipcMain.on('postage', function (event, arg) {
 	if (arg === 'restart') {
@@ -113,6 +122,17 @@ ipcMain.on('postage', function (event, arg) {
 		});
 		connectionWindowState.manage(connectionWindow);
 		connectionWindow.loadURL('file://' + app.getAppPath() + '/postage/web_root/postage/app/config.html?file=postage-connections.conf',  { 'extraHeaders': 'pragma: no-cache\n' });
+	} else if (arg === 'edit PGPASS') {
+		pgpassWindow = new BrowserWindow({
+			'x': pgpassWindowState.x,
+			'y': pgpassWindowState.y,
+			'width': pgpassWindowState.width,
+			'height': pgpassWindowState.height
+		});
+		pgpassWindowState.manage(pgpassWindow);
+		pgpassWindow.loadURL('file://' + app.getAppPath() + '/postage/web_root/postage/app/config.html?file=PGPASS',  { 'extraHeaders': 'pragma: no-cache\n' });
+		pgpassWindow.webContents.on('will-navigate', handleRedirect);
+		pgpassWindow.webContents.on('new-window', handleRedirect);
 	}
 })
 
@@ -155,6 +175,21 @@ function setMenu() {
 						});
 						connectionWindowState.manage(connectionWindow);
 						connectionWindow.loadURL('file://' + app.getAppPath() + '/postage/web_root/postage/app/config.html?file=postage-connections.conf',  { 'extraHeaders': 'pragma: no-cache\n' });
+					}
+				},
+				{
+					label: 'Edit PGPASS',
+					click: function () {
+						pgpassWindow = new BrowserWindow({
+							'x': pgpassWindowState.x,
+							'y': pgpassWindowState.y,
+							'width': pgpassWindowState.width,
+							'height': pgpassWindowState.height
+						});
+						pgpassWindowState.manage(pgpassWindow);
+						pgpassWindow.loadURL('file://' + app.getAppPath() + '/postage/web_root/postage/app/config.html?file=PGPASS',  { 'extraHeaders': 'pragma: no-cache\n' });
+						pgpassWindow.webContents.on('will-navigate', handleRedirect);
+						pgpassWindow.webContents.on('new-window', handleRedirect);
 					}
 				},
 				{
@@ -313,6 +348,9 @@ function openWindow() {
 	curWindow.on('closed', function () {
 		mainWindows.splice(mainWindows.indexOf(curWindow), 1);
 	});
+
+	curWindow.webContents.on('will-navigate', handleRedirect);
+	curWindow.webContents.on('new-window', handleRedirect);
 }
 
 function appStart() {
@@ -337,6 +375,13 @@ function appStart() {
 		defaultHeight: 768,
 		path: os.homedir() + '/.postage/',
 		file: 'connection-window-state.json'
+	});
+
+	pgpassWindowState = windowStateKeeper({
+		defaultWidth: 1024,
+		defaultHeight: 768,
+		path: os.homedir() + '/.postage/',
+		file: 'pgpass-window-state.json'
 	});
 
 	openWindow();
