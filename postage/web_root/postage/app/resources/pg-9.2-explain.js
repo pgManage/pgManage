9b169d4e68586b1e946eb2d10d676f7e9d2214a4
Nunzio Tocci <nunzio@tocci.org>
2017-03-31 17:40:36 -0500

    working on explain (for workflowproducts/postage#176)

--- postage/web_root/postage/app/resources/pg-9.2-explain.js
+++ postage/web_root/postage/app/resources/pg-9.2-explain.js
@@ -169,15 +169,25 @@ function explain(bolRun) {
                     if (data.strMessage !== '\\.') {
                         if (data.intCallbackNumber === 0) {
                             resultsContainer.innerHTML =
-								'<div style="width: 100%; height: 100%;" flex-vertical>' +
+								'<div style="width: 100%; height: 100%; position: relative;" flex-vertical>' +
 									'<iframe style="width: 100%; border: none;" flex src="frames/frame-explain.html"></iframe>' +
+									'<gs-button remove-all class="button-pop-out-explain" icononly icon="external-link" inline></gs-button>' +
 								'</div>' +
 								'';
 							var explainFrame = resultsContainer.children[0].children[0];
-							console.log(explainFrame);
+							var popoutButton = resultsContainer.children[0].children[1];
+
+							popoutButton.addEventListener('click', function () {
+								var newWindow = window.open('frames/frame-explain.html', 'ExplainQuery' + CryptoJS.MD5(strRunQuery), 'menubar=off');
+								newWindow.onload = function () {
+									GS.triggerEvent(newWindow, 'data-ready', {
+										explainJSON: JSON.parse(GS.decodeFromTabDelimited(data.strMessage)),
+										bolRun: bolRun
+									});
+								};
+							});
 
 							explainFrame.onload = function () {
-								console.log(explainFrame.onload);
 								GS.triggerEvent(explainFrame.contentWindow, 'data-ready', {
 									explainJSON: JSON.parse(GS.decodeFromTabDelimited(data.strMessage)),
 									bolRun: bolRun
