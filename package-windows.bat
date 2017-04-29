67ab760f6dd293b881848be15f0f3cfbc05cc365
Nunzio Tocci <nunzio@tocci.org>
2017-04-29 17:07:23 -0500

    windows package script

---
+++ package-windows.bat
@@ -0,0 +1,16 @@
+C:\Users\nunzio\AppData\Local\Programs\Git\bin\git.exe pull
+
+msbuild visualstudio\postage.sln /target:"postage:Clean" /verbosity:normal /p:Configuration=Release
+msbuild visualstudio\postage.sln /target:postage /verbosity:normal /p:Configuration=Release
+cd postage_electron
+
+del /S /Q app\postage
+mkdir app\postage
+mkdir app\postage\web_root
+mkdir app\postage\config
+xcopy ..\visualstudio\Release\postage.exe app\postage\
+xcopy ..\visualstudio\lib\x86\* app\postage\
+xcopy ..\postage\web_root app\postage\web_root /E
+xcopy ..\postage\config app\postage\config /E
+
+npm run dist-win
--- postage_electron/update.bat
+++
@@ -1,8 +0,0 @@
-del /S /Q app\postage
-mkdir app\postage
-mkdir app\postage\web_root
-mkdir app\postage\config
-xcopy ..\visualstudio\Release\postage.exe app\postage\
-xcopy ..\visualstudio\lib\x86\* app\postage\
-xcopy ..\postage\web_root app\postage\web_root /E
-xcopy ..\postage\config app\postage\config /E
