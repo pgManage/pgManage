56476ef9aa2d9f9093d170eaaaac848384633822
Nunzio Tocci <nunzio@tocci.org>
2017-05-02 15:51:55 -0500

    make test.sh use gmake on bsds

--- postage/test.sh
+++ postage/test.sh
@@ -2,14 +2,26 @@
 
 set -e
 
+if test $(uname -s) = "Linux"; then
+	MAKE=make
+elif test $(uname -s) = "Darwin"; then
+	MAKE=make
+elif test $(uname -s) = "FreeBSD"; then
+	MAKE=gmake
+elif test $(uname -s) = "OpenBSD"; then
+	MAKE=gmake
+else
+	MAKE=make
+fi
+
 rm -rf postage-master master.zip ~/.mozilla ~/.postage
 
 wget https://github.com/workflowproducts/postage/archive/master.zip
 unzip master.zip
 cd postage-master/
 
-./configure && make -j32
-make test-common
+./configure && $MAKE -j32
+$MAKE test-common
 
 ./postage/postage -c ./postage/config/postage.conf -d ./postage/config/postage-connections.conf -n true -t 300 -r ./postage/web_root -l info & export POSTAGEPID="$!"
 sleep 5
@@ -19,7 +31,7 @@ kill $POSTAGEPID
 
 rm -rf ~/.mozilla ~/.postage
 
-sudo make install
+sudo $MAKE install
 
 /usr/local/sbin/postage & export POSTAGEPID="$!"
 sleep 5
@@ -27,7 +39,7 @@ xdg-open "http://127.0.0.1:8080/postage/test/index.html?seq_numbers=true&_http_a
 printf "HTTP/1.1 200 OK\r\n\r\n\r\n" | ncat -l -p 45654
 kill $POSTAGEPID
 
-sudo make uninstall
+sudo $MAKE uninstall
 
 kill -9 $(cat postage/postgres.pid)
 
