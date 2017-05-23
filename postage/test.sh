a6f99fac19170dce0df0fe06fe2900020a7cae88
Nunzio Tocci <nunzio@tocci.org>
2017-05-23 15:53:28 -0500

    testing

--- postage/test.sh
+++ postage/test.sh
@@ -69,6 +69,10 @@ if test $(uname -s) = "Linux"; then
 	}
 
 #elif test $(uname -s) = "Darwin"; then
+	./dist/mac/Postage.app/Contents/MacOS/Postage --postage-test & export POSTAGEPID="$!"
+	printf "HTTP/1.1 200 OK\r\n\r\n\r\n" | ncat -l -p 45654
+	kill $POSTAGEPID
+	cp -f ./dist/*.dmp /mnt/Groups/wfprod_group/postage/packages/
 
 #elif test $(uname -s) = "FreeBSD"; then
 
