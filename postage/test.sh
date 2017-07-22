8d05cd8c224dd9c4cc16105fa52a10bc6c1c809b
Nunzio Tocci <nunzio@tocci.org>
2017-07-22 10:30:10 -0500

    Update test.sh

--- postage/test.sh
+++ postage/test.sh
@@ -23,7 +23,7 @@ cd postage-master/
 ./configure && $MAKE -j32
 $MAKE test-common
 
-./postage/postage -c ./postage/config/postage.conf -d ./postage/config/postage-connections.conf -n true -t 300 -r ./postage/web_root -l info & export POSTAGEPID="$!"
+./postage/postage -c ./postage/config/postage.conf -d ./postage/config/postage-connections-test.conf -n true -t 300 -r ./postage/web_root -l info & export POSTAGEPID="$!"
 sleep 5
 xdg-open "http://127.0.0.1:8080/postage/test/index.html?seq_numbers=true&_http_auth=true&http_file=true&http_upload=true&http_export=true&ws_raw=true&ws_tab=true&ws_select=true&ws_insert=true&ws_update=true&ws_delete=true" &
 printf "HTTP/1.1 200 OK\r\n\r\n\r\n" | ncat -l -p 45654
@@ -33,7 +33,7 @@ rm -rf ~/.mozilla ~/.postage
 
 sudo $MAKE install
 
-/usr/local/sbin/postage & export POSTAGEPID="$!"
+/usr/local/sbin/postage -d ./postage/config/postage-connections-test.conf & export POSTAGEPID="$!"
 sleep 5
 xdg-open "http://127.0.0.1:8080/postage/test/index.html?seq_numbers=true&_http_auth=true&http_file=true&http_upload=true&http_export=true&ws_raw=true&ws_tab=true&ws_select=true&ws_insert=true&ws_update=true&ws_delete=true" &
 printf "HTTP/1.1 200 OK\r\n\r\n\r\n" | ncat -l -p 45654
