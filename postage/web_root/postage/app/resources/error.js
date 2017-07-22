b18260670602780456319a3cd22cce43d01f9575
michaeltocci <michael@tocci.org>
2017-07-22 10:24:52 -0500

    Fix error.js

--- postage/web_root/postage/app/resources/error.js
+++ postage/web_root/postage/app/resources/error.js
@@ -1,5 +1,5 @@
 window.onerror = function (msg, url, lineNo, columnNo, error) {
     var errormsg = '\n    msg: ' + msg +  '\n    url: ' + url +  '\n    lineNo: ' + lineNo +  '\n    columnNo: ' + columnNo + '\n    error: ' + error + '';
-    //GS.ajaxJSON('https://www.workflowproducts.com/env/postage.acceptnc_error_eml', 'error=' + errormsg + '&version=' + ((window.contextData && contextData.postageVersion) ? contextData.postageVersion : '') + '&postgresversion=' + ((window.contextData && contextData.versionNumber) ? contextData.versionNumber : '') + '', function (data, error) {
-    //});
+    GS.ajaxJSON('https://www.workflowproducts.com/env/postage.acceptnc_error_eml', 'error=' + errormsg + '&version=' + ((window.contextData && contextData.postageVersion) ? contextData.postageVersion : '') + '&postgresversion=' + ((window.contextData && contextData.versionNumber) ? contextData.versionNumber : '') + '', function (data, error) {
+    });
 }
